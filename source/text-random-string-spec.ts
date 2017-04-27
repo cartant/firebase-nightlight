d385359b16a49b89b1d4f15f7f8f72d1411d0fd8
Nicholas Jamieson <nicholas@cartant.com>
2017-04-27 15:36:07 +1000

    chore(doc): Change license abbrev.

--- source/text-random-string-spec.ts
+++ source/text-random-string-spec.ts
@@ -1,6 +1,6 @@
 /**
  * @license Copyright © 2017 Nicholas Jamieson. All Rights Reserved.
- * Use of this source code is governed by a GPLv3 license that can be
+ * Use of this source code is governed by a GPL-3.0 license that can be
  * found in the LICENSE file at https://github.com/cartant/firebase-key
  */
 /*tslint:disable:no-unused-expression*/
